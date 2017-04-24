54eba4a9c7d542c6f771cca6b00956da7f91aa4e
enriquemore587 <jose.vergara2104@hotmail.com>
2017-04-23 19:34:33 -0500

    Update configuracion.php

--- configuracion.php
+++ configuracion.php
@@ -1,7 +1,7 @@
 <?php
-	define('DB_HOST','localhost');
-	define('DB_USUARIO','root');
-	define('DB_CONTRA','');
-	define('DB_NOMBRE','principal');
-	define('DB_CHARSET','utf8');
+	define('DB_HOST','DireccionServer');#ejemplo 127.0.0.1
+	define('DB_USUARIO','usuario');
+	define('DB_CONTRA','contraseña');
+	define('DB_NOMBRE','nombre_de_la_base_de_datos');
+	define('DB_CHARSET','utf8');#codificacion utf-8
 ?>
