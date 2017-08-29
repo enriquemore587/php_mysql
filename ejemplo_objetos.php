d900824c49186353b7d9ee0d50aea3e89c92af8c
enriquemore587 <jose.vergara2104@hotmail.com>
2017-08-29 12:58:36 -0500

    Update ejemplo_objetos.php

--- ejemplo_objetos.php
+++ ejemplo_objetos.php
@@ -1,1 +1,15 @@
 
+<?php
+
+
+  $obj_boleano = true;   // a boolean
+  $obj_tipostring = 'foo';    // a string
+
+  $cel1 = new Celular("Rojo", 2);
+  $cel1->llamar("5512345678");
+
+  $cel2 = new Celular("Negro", 1);
+  $cel2->sonar();
+
+
+?>
